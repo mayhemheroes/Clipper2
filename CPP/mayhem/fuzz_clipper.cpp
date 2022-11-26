17a08214e8c5d9c4761837da9bc0589ccf8a85ba
bcapuano <bcapuano@asu.edu>
2022-11-26 16:14:07 -0500

    Better

--- CPP/mayhem/fuzz_clipper.cpp
+++ CPP/mayhem/fuzz_clipper.cpp
@@ -13,12 +13,6 @@ using namespace Clipper2Lib;
 // Some constants needed for fuzzing decisions
 constexpr const int max_fill_rule = 3;
 
-void fuzz_shape_creation(FuzzedDataProvider &fdp) {
-    Rect64 rect{fdp.ConsumeIntegral<int64_t>(), fdp.ConsumeIntegral<int64_t>(),
-                fdp.ConsumeIntegral<int64_t>(), fdp.ConsumeIntegral<int64_t>()};
-    auto ellipse = Ellipse(rect, fdp.ConsumeIntegral<uint8_t>());
-}
-
 void fuzz_path_function(FuzzedDataProvider &fdp) {
     Paths64 subject, clip, solution;
     auto fill_rule = static_cast<FillRule>(fdp.ConsumeIntegralInRange(0, max_fill_rule));
@@ -41,18 +35,11 @@ void fuzz_path_function(FuzzedDataProvider &fdp) {
             Xor(subject, clip, fill_rule);
             break;
     }
-
 }
 
 extern "C" [[maybe_unused]] int LLVMFuzzerTestOneInput(const uint8_t *data, std::size_t size) {
     FuzzedDataProvider fdp(data, size);
-    auto fuzz_shapes = fdp.ConsumeBool();
-
-    if (fuzz_shapes) {
-        fuzz_shape_creation(fdp);
-    } else {
-        fuzz_path_function(fdp);
-    }
+    fuzz_path_function(fdp);
 
     return 0;
 }
